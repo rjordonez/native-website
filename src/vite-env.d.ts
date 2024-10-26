fcd8e177382a5763aec7e2683ad5502fb0af1bb7
Rex Ordonez <rexordonez@Rexs-MacBook-Pro.local>
2024-10-26 07:34:22 -0700

    Set up GitHub Pages

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/native-website/' // Replace 'repository' with your actual GitHub repository name
+});
